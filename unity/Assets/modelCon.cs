208631ac5158800aa1fb3d4175e4f9e3e93ec374
kappa1116jp <kappa1116jp@gmail.com>
2013-11-10 05:35:35 +0900

    ・かごの移動処理をプログラムに変更 ・かごがエンドに到達すると下に落下する処理作成

--- unity/Assets/modelCon.cs
+++ unity/Assets/modelCon.cs
@@ -3,6 +3,12 @@ using System.Collections;
 
 public class modelCon : MonoBehaviour {
 
+	private RaycastHit	hit;				//hitオブジェクト取得;
+	public GameObject Box;
+	public float m_Speed = 1;
+	public float m_DownSpeed = 8;
+	private bool LaneEndFlg = false;
+	
 	// Use this for initialization
 	void Start () {
 	
@@ -10,7 +16,26 @@ public class modelCon : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		transform.Translate(-Vector3.forward * Time.deltaTime);
-	
+
+		Physics.Raycast(this.transform.position , Box.transform.up ,out hit,100);
+//		Debug.Log(hit.transform);
+		if (hit.transform != null){
+			if (hit.transform.gameObject.tag == "LaneEnd"){
+				LaneEndFlg = true;
+			}
+		}	
+		else{
+		transform.Translate(-Vector3.forward * Time.deltaTime * m_Speed);
+		}
+		
+		if (LaneEndFlg){
+			transform.Translate(Vector3.down * Time.deltaTime * m_DownSpeed);
+			if (this.transform.position.y < -50)
+			{
+				Destroy(gameObject);
+			}
+		}
 	}
+	
+	
 }
