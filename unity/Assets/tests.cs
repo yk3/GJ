079c1d7b616fdedac3376d719ccb7bb5340f5b39
yk3 <hamayu5456@yahoo.co.jp>
2013-11-09 14:12:25 +0900

    tests.cs edited

--- unity/Assets/tests.cs
+++ unity/Assets/tests.cs
@@ -1,15 +1,17 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class tests : MonoBehaviour {
-
+	
+	public float suuji = 30.0f;
+	
 	// Use this for initialization
 	void Start () {
-	
+		Debug.Log("test");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
 	}
 }
